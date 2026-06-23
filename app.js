3b927c15b595bb552851d1b9488f3233fab2991f
To Tieu Han <hanb2308354@student.ctu.edu.vn>
2026-06-24 05:22:32 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -9,5 +9,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
-
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
